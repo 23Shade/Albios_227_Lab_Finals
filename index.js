39f6cd606f0d14872d33b4ac02c28889fdb126da
Shade <shadealbios@gmail.com>
2023-06-21 00:29:49 +0800

    fix error

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/api/userRoutes');
-const productRoutes = require('./routes/api/productRoutes');
-const orderRoutes = require('./routes/api/orderRoutes');
-const cartRoutes = require('./routes/api/cartRoutes'); // Import cart routes
 
 const PORT = 3000;
 const app = express();
@@ -14,14 +10,10 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost/ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 });
 
 // Routes
 app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/cart', cartRoutes); // Mount cart routes
 
 // Error handling middleware
 app.use((err, req, res, next) => {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../../controllers/userController');
 
-router.post('/register', userController.registerUser);
-router.post('/login', userController.authenticateUser);
-router.put('/set-admin', userController.setAdmin);
+// Middleware to validate user
+const validateUser = (req, res, next) => {
+  // Check if user is authenticated
+  const isAuthenticated = true; // Assuming the user is authenticated
 
+  if (isAuthenticated) {
+    req.user = { id: 1, name: 'John', isAdmin: true }; // Sample user data
+    next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+// Middleware to check if user is admin
+const isAdmin = (req, res, next) => {
+  // Check if the authenticated user is an admin
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: 'Forbidden' });
+  }
+};
+
+router.get('/', (req, res) => {
+  res.send('User routes');
+});
+
+// Export the router
 module.exports = router;
