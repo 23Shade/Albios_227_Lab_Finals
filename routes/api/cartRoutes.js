339515f7065b64614eb51fd202ec330810fd39ab
Shade <shadealbios@gmail.com>
2023-06-20 23:59:04 +0800

    add cartRoutes

--- routes/api/cartRoutes.js
+++ routes/api/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const cartController = require('../controllers/cartController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const cartController = require('../../controllers/cartController');
+const authMiddleware = require('../../middlewares/authMiddleware');
 
 // Add product to cart
 router.post('/cart', authMiddleware.validateUser, cartController.addToCart);
